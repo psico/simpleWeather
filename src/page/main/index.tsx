20528a58b8342ab4f280d378111c4c2ee68661cb
psico <desenvolvedorjg@gmail.com>
2022-11-19 10:31:53 -0300

    [UPT] Coding background collor

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -2,7 +2,7 @@ import {Text, View} from 'react-native';
 
 const Main = () => {
   return (
-    <View>
+    <View style={{backgroundColor: '#36c7ff'}}>
       <Text>Hello Main Page</Text>
     </View>
   );
