400779ae0de46d00b91980c8f31c3b07e86617a5
psico <desenvolvedorjg@gmail.com>
2022-11-21 00:06:19 -0300

    [UPT] Beggining to code api request to weather.

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,7 +1,32 @@
 import {Text, View} from 'react-native';
 import Styles from './css';
+import axios from 'axios';
 
 const Main = () => {
+  const encodedParams = new URLSearchParams();
+  encodedParams.append('apiKey', '<REQUIRED>');
+  encodedParams.append('locationKey', '<REQUIRED>');
+
+  const options = {
+    method: 'POST',
+    url: 'https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': '0171484c54mshe7b720ea0ed42e1p1f5c22jsndceb0b513c61',
+      'X-RapidAPI-Host': 'AccuWeatherstefan-skliarovV1.p.rapidapi.com',
+    },
+    data: encodedParams,
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
   return (
     <View style={Styles.body}>
       <Text>Hello Main Page</Text>
