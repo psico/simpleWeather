d62326b5a8bf80a1526d84a2905f786c22d30997
psico <desenvolvedorjg@gmail.com>
2022-12-05 23:23:15 -0300

    [UPT] Trying to use api

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -12,7 +12,7 @@ const Main = () => {
     url: 'https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey',
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
-      'X-RapidAPI-Key': '0171484c54mshe7b720ea0ed42e1p1f5c22jsndceb0b513c61',
+      'X-RapidAPI-Key': '24d81cf917mshf57f1c543de525ep1d11d2jsn5809bb93c57a',
       'X-RapidAPI-Host': 'AccuWeatherstefan-skliarovV1.p.rapidapi.com',
     },
     data: encodedParams,
