a3ec56124f680fe47c155dab602a23954641769c
psico <desenvolvedorjg@gmail.com>
2022-12-07 17:19:48 -0300

    [UPT] Trying to use api

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -6,6 +6,32 @@ const Main = () => {
   const encodedParams = new URLSearchParams();
   console.log('console.log(response.data); console.log(response.data);');
 
+  // encodedParams.append(
+  //   'apiKey',
+  //   '24d81cf917mshf57f1c543de525ep1d11d2jsn5809bb93c57a',
+  // );
+  // encodedParams.append('locationKey', 'Brasilia');
+  //
+  // const options = {
+  //   method: 'POST',
+  //   url: 'https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey',
+  //   headers: {
+  //     'content-type': 'application/x-www-form-urlencoded',
+  //     'X-RapidAPI-Key': '24d81cf917mshf57f1c543de525ep1d11d2jsn5809bb93c57a',
+  //     'X-RapidAPI-Host': 'AccuWeatherstefan-skliarovV1.p.rapidapi.com',
+  //   },
+  //   data: encodedParams,
+  // };
+  //
+  // axios
+  //   .request(options)
+  //   .then(function (response) {
+  //     console.log('right => ', response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.error('wrong => ', error);
+  //   });
+
   encodedParams.append(
     'apiKey',
     '24d81cf917mshf57f1c543de525ep1d11d2jsn5809bb93c57a',
@@ -13,23 +39,17 @@ const Main = () => {
   encodedParams.append('locationKey', 'Brasilia');
 
   const options = {
-    method: 'POST',
-    url: 'https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'X-RapidAPI-Key': '24d81cf917mshf57f1c543de525ep1d11d2jsn5809bb93c57a',
-      'X-RapidAPI-Host': 'AccuWeatherstefan-skliarovV1.p.rapidapi.com',
-    },
-    data: encodedParams,
+    method: 'GET',
+    url: 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}',
   };
 
   axios
     .request(options)
     .then(function (response) {
-      console.log(response.data);
+      console.log('right => ', response.data);
     })
     .catch(function (error) {
-      console.error(error);
+      console.error('wrong => ', error);
     });
 
   return (
