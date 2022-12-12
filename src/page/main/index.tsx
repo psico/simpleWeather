2e75419dbd9b688f0836be765bb5ba1b73bf7980
psico <desenvolvedorjg@gmail.com>
2022-12-12 09:38:44 -0300

    [UPT] Using .env variable to api weather

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,13 +1,15 @@
 import {Text, View} from 'react-native';
 import Styles from './css';
 import axios from 'axios';
+import {REACT_APP_OPEN_WEATHER_API_KEY} from '@env';
 
 const Main = () => {
-  console.info('Main component');
+  // @ts-ignore
+  console.info('Main component', REACT_APP_OPEN_WEATHER_API_KEY);
 
   const options = {
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}',
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${REACT_APP_OPEN_WEATHER_API_KEY}`,
   };
 
   axios
