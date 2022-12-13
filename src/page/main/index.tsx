6212a552ebda360376ecdd31a52d7ebbbaffe65c
psico <desenvolvedorjg@gmail.com>
2022-12-13 09:17:01 -0300

    [ADD] Getting current position

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,11 +1,66 @@
-import {Text, View} from 'react-native';
+import {Button, PermissionsAndroid, Platform, Text, View} from 'react-native';
 import Styles from './css';
 import axios from 'axios';
 import {REACT_APP_OPEN_WEATHER_API_KEY} from '@env';
+import Geolocation from '@react-native-community/geolocation';
+import {useState} from 'react';
 
 const Main = () => {
-  // @ts-ignore
   console.info('Main component', REACT_APP_OPEN_WEATHER_API_KEY);
+
+  const [currentLatitude, setCurrentLatitude] = useState('');
+  const [currentLongitude, setCurrentLongitude] = useState('');
+  const [watchID, setWatchID] = useState(0);
+
+  const callLocation = () => {
+    if (Platform.OS === 'ios') {
+      getLocation();
+    } else {
+      const requestLocationPermission = async () => {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Permissão de Acesso à Localização',
+            message: 'Este aplicativo precisa acessar sua localização.',
+            buttonNeutral: 'Pergunte-me depois',
+            buttonNegative: 'Cancelar',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getLocation();
+        } else {
+          console.warn('Permissão de Localização negada');
+        }
+      };
+
+      requestLocationPermission().then(() => ({}));
+    }
+  };
+
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+        setCurrentLatitude(currentLatitude);
+        setCurrentLongitude(currentLongitude);
+      },
+      error => console.warn('deu erro', error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+    const watchID = Geolocation.watchPosition(position => {
+      const currentLatitude = JSON.stringify(position.coords.latitude);
+      const currentLongitude = JSON.stringify(position.coords.longitude);
+      setCurrentLatitude(currentLatitude);
+      setCurrentLongitude(currentLongitude);
+    });
+    setWatchID(watchID);
+  };
+
+  const clearLocation = () => {
+    Geolocation.clearWatch(watchID);
+  };
 
   const options = {
     method: 'GET',
@@ -24,6 +79,15 @@ const Main = () => {
   return (
     <View style={Styles.body}>
       <Text>Hello Main Page</Text>
+      <Text>Você está Aqui</Text>
+      <Text>Latitude: {currentLatitude}</Text>
+      <Text>Longitude: {currentLongitude}</Text>
+      <View>
+        <Button title="Obter Localização" onPress={callLocation} />
+      </View>
+      <View>
+        <Button title="Cancelar Monitoração" onPress={clearLocation} />
+      </View>
     </View>
   );
 };
