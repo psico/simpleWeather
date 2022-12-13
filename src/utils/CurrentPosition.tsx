d7a49f18ba3241c9f1300d1c53142fb4a8b4b965
psico <desenvolvedorjg@gmail.com>
2022-12-13 10:23:25 -0300

    [ADD] Improving get current position

---
+++ src/utils/CurrentPosition.tsx
@@ -0,0 +1,71 @@
+import {useEffect, useState} from 'react';
+import {PermissionsAndroid, Platform} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+
+const CurrentPosition = () => {
+  const [currentLatitude, setCurrentLatitude] = useState('');
+  const [currentLongitude, setCurrentLongitude] = useState('');
+  const [watchID, setWatchID] = useState(0);
+
+  const callLocation = () => {
+    if (Platform.OS === 'ios') {
+      getLocation();
+    } else {
+      const requestLocationPermission = async () => {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Permissão de Acesso à Localização',
+            message: 'Este aplicativo precisa acessar sua localização.',
+            buttonNeutral: 'Pergunte-me depois',
+            buttonNegative: 'Cancelar',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getLocation();
+        } else {
+          console.warn('Permissão de Localização negada');
+        }
+      };
+
+      requestLocationPermission().then(() => ({}));
+    }
+  };
+
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+        setCurrentLatitude(currentLatitude);
+        setCurrentLongitude(currentLongitude);
+      },
+      error => console.warn('deu erro', error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+    const watchID = Geolocation.watchPosition(position => {
+      const currentLatitude = JSON.stringify(position.coords.latitude);
+      const currentLongitude = JSON.stringify(position.coords.longitude);
+      setCurrentLatitude(currentLatitude);
+      setCurrentLongitude(currentLongitude);
+    });
+    setWatchID(watchID);
+  };
+
+  const clearLocation = () => {
+    Geolocation.clearWatch(watchID);
+  };
+
+  useEffect(() => {
+    callLocation();
+  });
+
+  return {
+    currentLatitude,
+    currentLongitude,
+    clearLocation,
+  };
+};
+
+export default CurrentPosition;
