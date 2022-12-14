1a81902f7bddeaead9a36f9febe5bdd46a693d49
psico <desenvolvedorjg@gmail.com>
2022-12-14 17:19:49 -0300

    [UPT] Requesting wheater to current position

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,37 +1,36 @@
-import {Button, PermissionsAndroid, Platform, Text, View} from 'react-native';
+import React, {Text, View} from 'react-native';
 import Styles from './css';
 import axios from 'axios';
 import {REACT_APP_OPEN_WEATHER_API_KEY} from '@env';
 import CurrentPosition from '../../utils/CurrentPosition';
+import {useEffect} from 'react';
 
 const Main = () => {
   console.info('Main component', REACT_APP_OPEN_WEATHER_API_KEY);
-
   const {currentLatitude, currentLongitude} = CurrentPosition();
 
-  // const options = {
-  //   method: 'GET',
-  //   url: `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${REACT_APP_OPEN_WEATHER_API_KEY}`,
-  // };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const getWeatherCurrentPosition = () => {
+    if (currentLatitude && currentLongitude) {
+      const options = {
+        method: 'GET',
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=${REACT_APP_OPEN_WEATHER_API_KEY}`,
+      };
 
-  console.log(
-    'URL => ',
-    `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=${REACT_APP_OPEN_WEATHER_API_KEY}`,
-  );
-
-  const options = {
-    method: 'GET',
-    url: `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=${REACT_APP_OPEN_WEATHER_API_KEY}`,
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log('right => ', response.data);
+        })
+        .catch(function (error) {
+          console.error('wrong => ', error);
+        });
+    }
   };
 
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log('right => ', response.data);
-    })
-    .catch(function (error) {
-      console.error('wrong => ', error);
-    });
+  useEffect(() => {
+    getWeatherCurrentPosition();
+  }, [getWeatherCurrentPosition]);
 
   return (
     <View style={Styles.body}>
