3a824a1dda9bb1bbb93a4725dbca146cac49e329
psico <desenvolvedorjg@gmail.com>
2022-12-16 17:29:07 -0300

    [UPT] Better layout

--- src/page/main/css.tsx
+++ src/page/main/css.tsx
@@ -2,7 +2,36 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   body: {
+    display: 'flex',
     flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textHeader: {
+    fontFamily: 'Trebuchet MS, Verdana, sans-serif',
+    fontSize: 30,
+    color: '#FFFFFF',
+  },
+  textSmall: {
+    fontFamily: 'Trebuchet MS, Verdana, sans-serif',
+    fontSize: 15,
+    color: '#FFFFFF',
+  },
+  textDefault: {
+    fontFamily: 'Trebuchet MS, Verdana, sans-serif',
+    fontSize: 20,
+    color: '#FFFFFF',
+  },
+  logo: {
+    width: 150,
+    height: 150,
   },
 });
 
