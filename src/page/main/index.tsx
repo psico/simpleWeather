e4f88c3013d35a8fd7a3c0c2083e40292c68b152
psico <desenvolvedorjg@gmail.com>
2022-12-21 14:30:28 -0300

    [ADD] adding icon svg

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,10 +1,14 @@
-import React, {Image, Text, View} from 'react-native';
+import React, {Text, View, Animated, Image} from 'react-native';
 import Styles from './css';
 import axios from 'axios';
 import {REACT_APP_OPEN_WEATHER_API_KEY} from '@env';
 import CurrentPosition from '../../utils/CurrentPosition';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import LinearGradient from 'react-native-linear-gradient';
+import ClearDay from '../../../public/weatherIcons/clear-day.svg';
+import Svg, {Path} from 'react-native-svg';
+
+const animatedValue = new Animated.Value(1);
 
 const Main = () => {
   console.info('Main component');
@@ -40,9 +44,35 @@ const Main = () => {
     }
   };
 
+  const interpolateRotation = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['360deg', '0deg'],
+  });
+
+  const animatedStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    transform: [{rotate: interpolateRotation}],
+  };
+
+  const startAnimation = () => {
+    animatedValue.setValue(0);
+    Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 1500,
+      useNativeDriver: true,
+      easing: () => 10,
+    }).start(() => startAnimation());
+  };
+
   useEffect(() => {
     getWeatherCurrentPosition();
   }, [currentLatitude, currentLongitude]);
+
+  useEffect(() => {
+    startAnimation();
+  }, []);
 
   return (
     <LinearGradient
@@ -50,15 +80,46 @@ const Main = () => {
       style={Styles.body}
       start={{x: 0.0, y: 0.15}}
       end={{x: 0.95, y: 0.75}}>
-      <View style={[Styles.transparenceMain, Styles.mainCard]}>
+      <View style={[Styles.transparenceMain, Styles.mainCard]} height="65%">
         <Text style={Styles.textHeader}>{weather?.name}</Text>
         <Text style={Styles.textSmall}>{new Date().toGMTString()}</Text>
-        <Image
-          style={Styles.logo}
-          source={{
-            uri: `https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`,
-          }}
-        />
+        {/*<Image*/}
+        {/*  style={Styles.logo}*/}
+        {/*  source={{*/}
+        {/*    uri: `https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`,*/}
+        {/*  }}*/}
+        {/*/>*/}
+
+        {/*<View style={Styles.logo}>*/}
+        {/*  <Svg*/}
+        {/*    height="100%"*/}
+        {/*    width="100%"*/}
+        {/*    viewBox="1 -1 30 30"*/}
+        {/*    fill="none"*/}
+        {/*    xmlns="http://www.w3.org/2000/svg">*/}
+        {/*    <ClearDay />*/}
+        {/*  </Svg>*/}
+        {/*</View>*/}
+
+        <View style={Styles.logo}>
+          <ClearDay height="100%" width="100%" viewBox="1 -1 30 30" />
+        </View>
+
+        {/*<Animated.View style={[Styles.logo, animatedStyle]}>*/}
+        {/*  <ClearDay height="100%" width="100%" viewBox="1 -1 30 30" />*/}
+        {/*</Animated.View>*/}
+
+        {/*<Animated.View style={Styles.logo}>*/}
+        {/*  <ClearDay height="100%" width="100%" viewBox="1 -1 30 30" />*/}
+        {/*</Animated.View>*/}
+
+        {/*<View style={Styles.logo}>*/}
+        {/*  <LocalSvg*/}
+        {/*    width="100%"*/}
+        {/*    height="100%"*/}
+        {/*    xml='../../../public/weatherIcons/clear-day.svg'*/}
+        {/*  />*/}
+        {/*</View>*/}
         <Text style={Styles.textHeader}>
           {weather?.main?.temp}º {weather?.weather[0]?.main}
         </Text>
